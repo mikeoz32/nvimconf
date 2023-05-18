6da754bb24101ce2d442e2f12ef29d21628863d0
mykhailo kozlovskyi <mykhailo.kozlovskyi@asta.mobi>
2023-05-18 13:13:18 +0300

    added default term

--- lua/oz/terminals.lua
+++ lua/oz/terminals.lua
@@ -5,10 +5,20 @@ local lazy_git = Terminal:new({
   direction = "float"
 })
 
+local base_term = Terminal:new({})
+
 local Terminals = {}
 
 Terminals.lazygit_toggle = function ()
   lazy_git:toggle()
+end
+
+Terminals.toggle_term = function ()
+  base_term:toggle()
+end
+
+Terminals.setup = function ()
+  
 end
 
 return Terminals
